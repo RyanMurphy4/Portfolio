742e33bf6a9b2e98c7da9972294650bc92a02699
RyanMurphy4 <108695175+RyanMurphy4@users.noreply.github.com>
2024-10-08 22:12:04 -0400

    Create Nav Bar
    Alter view for project_list

--- my_portfolio/portfolio_app/urls.py
+++ my_portfolio/portfolio_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path('projects', views.projects, name='portfolio-projects'),
+    path("", views.index, name='home'),
+    path('projects', views.project_list, name='portfolio-projects'),
 ]
--- my_portfolio/portfolio_app/views.py
+++ my_portfolio/portfolio_app/views.py
@@ -3,12 +3,34 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    numbers = [i for i in range(100)]
+    numbers = [i for i in range(10)]
     return render(request, 'portfolio_app/index.html', {
         "nums": numbers
     })
 
-def projects(request):
+def project_list(request):
+
+    projects = [
+    {
+        'title': "Project name",
+
+        "description": 
+        
+        '''
+        Long project description
+        is going to be here
+        on multiple lines
+        ..weird
+        smile
+        ''',
+
+        "image_url" : 'portfolio_app/static/portfolio_app/images/project1.png',  #OPTIONAL
+        "github_url": 'GITHUB_URL',
+        "project_url": "ACTUAL WEBSITE URL"
+    },
+
+    ]
+
     return render(request, 'portfolio_app/projects.html', {
-        "nothing": "NOTHING"
+        "projects": projects
     })
